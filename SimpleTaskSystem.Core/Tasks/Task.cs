998d014ddabfae7b3bd5c521e07c37603215a923
zjding <zjding@outlook.com>
2014-10-01 14:15:38 -0500

    Entities

---
+++ SimpleTaskSystem.Core/Tasks/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using SimpleTaskSystem.People;
+
+namespace SimpleTaskSystem.Tasks
+{
+    public class Task : Entity<long>, IHasCreationTime
+    {
+        public virtual Person AssignedPerson { get; set; }
+        public virtual int? AssignedPersonId { get; set; }
+        public virtual string Description { get; set; }
+        public virtual DateTime CreationTime { get; set; }
+        public virtual TaskState State { get; set; }
+
+        public Task()
+        {
+            CreationTime = DateTime.Now;
+            State = TaskState.Active;
+        }
+    }
+}
---
+++ SimpleTaskSystem.Core/Tasks/TaskState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleTaskSystem.Tasks
+{
+    public enum TaskState : byte
+    {
+        /// <summary>
+        /// The task is active.
+        /// </summary>
+        Active = 1,
+
+        /// <summary>
+        /// The task is completed.
+        /// </summary>
+        Completed = 2
+    }
+}
